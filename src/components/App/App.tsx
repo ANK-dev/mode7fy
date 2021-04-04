05d5a6cb7d7e46ced379e0ed2660ff9ae6d90ebf
ANK-dev <arielnogueirak@gmail.com>
2021-04-03 21:58:16 -0300

    it works!

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Mode7 from '../Mode7/Mode7'
+import Controls from '../Mode7/Controls';
+
+function App() {
+  return (
+    <div className="App">
+      <Controls/>
+      <Mode7/>
+    </div>
+  );
+}
+
+export default App;
