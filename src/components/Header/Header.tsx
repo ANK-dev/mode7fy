b0a8e2e931f0317d15c5789cae602881fef2b388
ANK-dev <arielnogueirak@gmail.com>
2021-04-05 15:18:07 -0300

    improved responsiveness & added more controls

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,24 @@
-import { Navbar } from 'react-bootstrap';
+import React from 'react';
+import { Navbar, Button} from 'react-bootstrap';
 
-const Header: React.FC = () => {
+import { IStateSidebar } from '../App/App';
+
+import './Header.scss';
+
+interface IHeader {
+  sidebarState: IStateSidebar;
+  toggleSidebar: () => void;
+}
+
+const Header: React.FC<IHeader> = (props) => {
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      {
+        props.sidebarState.open
+          ? <Button id="menu" onClick={props.toggleSidebar}>&times;</Button>
+          : <Button id="menu" onClick={props.toggleSidebar}>+</Button>
+      }
       <Navbar.Brand className="font-italic mr-auto">MODE7FY</Navbar.Brand>
       <Navbar.Text><a href="https://github.com/ANK-dev">by ANK-dev</a></Navbar.Text>
     </Navbar>
